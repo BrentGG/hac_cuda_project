c4b187ed2c97a76660d4f25486e76c9af222916d
brent <brent.gerets@gmail.com>
2022-12-30 13:44:32 +0100

    added pooling on the cpu

--- opdrachtHAC.cu
+++ opdrachtHAC.cu
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "cuda.h"
 #include "cuda_runtime.h"
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image.h"
 #include "stb_image_write.h"
+
+#define POOLSTRIDE 2
 
 struct Pixel
 {
@@ -41,6 +44,53 @@ void convoluteCPU(unsigned char* input, unsigned char* output, int width, int he
             ptrPixel->b = sum[2];
             ptrPixel->a = opacity;
         }
+    }
+}
+
+void pool(unsigned char* input, unsigned char* outputMaxPool, unsigned char* outputMinPool, unsigned char* outputAvgPool, int width, int height, int poolStride) {
+    int poolWidth = (int)(width / POOLSTRIDE);
+    int poolHeight = (int)(height / POOLSTRIDE);
+    int row = 0;
+    for (int i = 0; i < height; i += poolStride) {
+        int col = 0;
+        for (int j = 0; j < width; j += poolStride) {
+            Pixel* p = (Pixel*)&input[i * width * 4 + 4 * j];
+            int max[3] = {p->r, p->g, p->b};
+            int min[3] = {p->r, p->g, p->b};
+            float avg[3] = {0, 0, 0};
+            for (int k = i; k < i + poolStride; ++k) {
+                for (int l = j; l < j + poolStride; ++l) {
+                    Pixel* q = (Pixel*)&input[k * width * 4 + 4 * l];
+                    int values[3] = {q->r, q->g, q->b};
+                    for (int v = 0; v < 3; ++v) {
+                        if (values[v] > max[v])
+                            max[v] = values[v];
+                        if (values[v] < min[v])
+                            min[v] = values[v];
+                        avg[v] += values[v];
+                    }
+                }
+            }
+            for (int v = 0; v < 3; ++v)
+                avg[v] /= poolStride * poolStride;
+            Pixel* q = (Pixel*)&outputMaxPool[row * poolWidth * 4 + 4 * col];
+            q->r = max[0];
+            q->g = max[1];
+            q->b = max[2];
+            q->a = 255;
+            Pixel* r = (Pixel*)&outputMinPool[row * poolWidth * 4 + 4 * col];
+            r->r = min[0];
+            r->g = min[1];
+            r->b = min[2];
+            r->a = 255;
+            Pixel* s = (Pixel*)&outputAvgPool[row * poolWidth * 4 + 4 * col];
+            s->r = round(avg[0]);
+            s->g = round(avg[1]);
+            s->b = round(avg[2]);
+            s->a = 255;
+            ++col;
+        }
+        ++row;
     }
 }
 
@@ -93,11 +143,31 @@ int main(int argc, char** argv)
     }
     printf(" DONE \r\n" );
 
-    // Process image on cpu
-    unsigned char* outputData = (unsigned char*) malloc(sizeof(unsigned char) * (width - 2) * (height - 2) * 4);
+    // Convolution on CPU
+    unsigned char* outputConvolution = (unsigned char*) malloc(sizeof(unsigned char) * (width - 2) * (height - 2) * 4);
     printf("Applying convolution...\r\n");
-    convoluteCPU(inputData, outputData, width, height, edgeDetection);
+    convoluteCPU(inputData, outputConvolution, width, height, edgeDetection);
     printf(" DONE \r\n");
+
+    // Pooling on GPU
+    int poolWidth = (int)(width / POOLSTRIDE);
+    int poolHeight = (int)(height / POOLSTRIDE);
+    printf("%d %d\n", width, height);
+    printf("%d %d\n", poolWidth, poolHeight);
+    unsigned char* outputMaxPool = (unsigned char*) malloc(sizeof(unsigned char) * poolWidth * poolHeight * 4);
+    unsigned char* outputMinPool = (unsigned char*) malloc(sizeof(unsigned char) * poolWidth * poolHeight * 4);
+    unsigned char* outputAvgPool = (unsigned char*) malloc(sizeof(unsigned char) * poolWidth * poolHeight * 4);
+    printf("Pooling...\r\n");
+    pool(inputData, outputMaxPool, outputMinPool, outputAvgPool, width, height, POOLSTRIDE);
+    printf(" DONE \r\n");
+
+    // Write images back to disk
+    printf("Writing pngs to disk...\r\n");
+    stbi_write_png("convolution.png", (width - 2), (height - 2), 4, outputConvolution, 4 * (width - 2));
+    stbi_write_png("maxpool.png", poolWidth, poolHeight, 4, outputMaxPool, 4 * poolWidth);
+    stbi_write_png("minpool.png", poolWidth, poolHeight, 4, outputMinPool, 4 * poolWidth);
+    stbi_write_png("avgpool.png", poolWidth, poolHeight, 4, outputAvgPool, 4 * poolWidth);
+    printf(" DONE\r\n");
 
     // Copy data to the gpu
     /*printf("Copy data to GPU...\r\n");
@@ -118,15 +188,11 @@ int main(int argc, char** argv)
     cudaMemcpy(inputData, ptrImageDataGpu, width * height * 4, cudaMemcpyDeviceToHost);
     printf(" DONE \r\n");*/
 
-    // Build output filename
-    const char * fileNameOut = "out.png";
-
-    // Write image back to disk
-    printf("Writing png to disk...\r\n");
-    stbi_write_png(fileNameOut, width, height, 4, outputData, 4 * width);
-    printf(" DONE\r\n");
-
     // Free memory
     //cudaFree(ptrImageDataGpu);
     stbi_image_free(inputData);
+    stbi_image_free(outputConvolution);
+    stbi_image_free(outputMaxPool);
+    stbi_image_free(outputMinPool);
+    stbi_image_free(outputAvgPool);
 }
