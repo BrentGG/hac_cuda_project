2e36b684fa4ecde952b8fefc89f515812cb41141
brent <brent.gerets@gmail.com>
2023-01-06 17:13:40 +0100

    load images from url

--- opdracht_c.cu
+++ opdracht_c.cu
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <curl/curl.h>
+#include <string.h>
 #include "cuda.h"
 #include "cuda_runtime.h"
 #define STB_IMAGE_IMPLEMENTATION
@@ -94,21 +96,55 @@ void pool(unsigned char* input, unsigned char* outputMaxPool, unsigned char* out
     }
 }
 
-__global__ void convoluteGPU(unsigned char* imageRGBA)
+static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
 {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    int idy = blockIdx.y * blockDim.y + threadIdx.y;
-
-    Pixel* ptrPixel = (Pixel*)&imageRGBA[idy * gridDim.x*blockDim.x * 4 + 4 * idx];
-    unsigned char pixelValue = (unsigned char)(ptrPixel->r * 0.2126f + ptrPixel->g * 0.7152f + ptrPixel->b * 0.0722f);
-    ptrPixel->r = pixelValue;
-    ptrPixel->g = pixelValue;
-    ptrPixel->b = pixelValue;
-    ptrPixel->a = 255;
+  size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+  return written;
 }
 
 int main(int argc, char** argv)
 {
+    // Image URLs and names
+    int imageAmount = 3;
+    char urls[imageAmount][1024] = {
+        "https://images.nintendolife.com/835d44208f0a6/mario-movie.large.jpg", // mario
+        "https://img.buzzfeed.com/buzzfeed-static/static/2022-03/16/0/enhanced/c1de3db394fb/original-1460-1647389279-4.png", // the rock
+        "https://images.squarespace-cdn.com/content/v1/5b788d28697a98e17a6d4c7a/b83f0eab-7dd6-4e9b-83a1-13139ac2a03b/rickroll+cropped.png", // rick astley
+    };
+    char names[imageAmount][20] = {
+        "image1.png",
+        "image2.png",
+        "image3.png"
+    };
+
+    // Get all the images from their URL
+    CURL *curl_handle;
+    char *imageFileName = (char*) malloc(sizeof(char) * 20);
+    FILE *imageFile;
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl_handle = curl_easy_init();
+    //curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
+    curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
+    for (int i = 0; i < imageAmount; ++i) {
+        printf("Looking for image %d...\n", i + 1);
+        curl_easy_setopt(curl_handle, CURLOPT_URL, urls[i]);
+        imageFile = fopen(names[i], "wb");
+        if(imageFile) {
+            curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, imageFile);
+            curl_easy_perform(curl_handle);
+            fclose(imageFile);
+        }
+        else {
+            printf(" FAILED\n");
+            return 1;
+        }
+        printf(" DONE\n");
+    }
+    curl_easy_cleanup(curl_handle);
+    curl_global_cleanup();
+
+    // Convolution kernel options
     float gaussianBlur[3][3] = {
         {0.0625*1, 0.0625*2, 0.0625*1},
         {0.0625*2, 0.0625*4, 0.0625*2},
@@ -125,72 +161,60 @@ int main(int argc, char** argv)
         {1, 0, -1}
     };
 
-    // Check argument count
-    if (argc < 2)
-    {
-        printf("Not enough arguments.");
-        return -1;
+    // Execute all the operations for every image
+    for(int f = 0; f < imageAmount; f++) {
+        // Open image
+        int width, height, componentCount;
+        printf("Loading png file %d...\n", f + 1);
+        unsigned char* inputData = stbi_load(names[f], &width, &height, &componentCount, 4);
+        if (!inputData) {
+            printf(" FAILED\n");
+            return -1;
+        }
+        printf(" DONE\n" );
+
+        // Convolution on CPU
+        unsigned char* outputConvolution = (unsigned char*) malloc(sizeof(unsigned char) * (width - 2) * (height - 2) * 4);
+        printf("Applying convolution...\n");
+        convoluteCPU(inputData, outputConvolution, width, height, edgeDetection);
+        printf(" DONE\n");
+
+        // Pooling on CPU
+        int poolWidth = (int)(width / POOLSTRIDE);
+        int poolHeight = (int)(height / POOLSTRIDE);
+        unsigned char* outputMaxPool = (unsigned char*) malloc(sizeof(unsigned char) * poolWidth * poolHeight * 4);
+        unsigned char* outputMinPool = (unsigned char*) malloc(sizeof(unsigned char) * poolWidth * poolHeight * 4);
+        unsigned char* outputAvgPool = (unsigned char*) malloc(sizeof(unsigned char) * poolWidth * poolHeight * 4);
+        printf("Pooling...\n");
+        pool(inputData, outputMaxPool, outputMinPool, outputAvgPool, width, height, POOLSTRIDE);
+        printf(" DONE\n");
+
+        // Write images back to disk
+        printf("Writing output pngs to disk...\n");
+        char buffer[255];
+        memset(buffer, '\0', sizeof(buffer));
+        strncpy(buffer, names[f], strlen(names[f]) - 4);
+        strcat(buffer, "_convolution.png");
+        stbi_write_png(buffer, width - 2, height - 2, 4, outputConvolution, 4 * width);
+        memset(buffer, '\0', sizeof(buffer));
+        strncpy(buffer, names[f], strlen(names[f]) - 4);
+        strcat(buffer, "_maxpool.png");
+        stbi_write_png(buffer, poolWidth, poolHeight, 4, outputMaxPool, 4 * poolWidth);
+        memset(buffer, '\0', sizeof(buffer));
+        strncpy(buffer, names[f], strlen(names[f]) - 4);
+        strcat(buffer, "_minpool.png");
+        stbi_write_png(buffer, poolWidth, poolHeight, 4, outputMinPool, 4 * poolWidth);
+        memset(buffer, '\0', sizeof(buffer));
+        strncpy(buffer, names[f], strlen(names[f]) - 4);
+        strcat(buffer, "_avgpool.png");
+        stbi_write_png(buffer, poolWidth, poolHeight, 4, outputAvgPool, 4 * poolWidth);
+        printf(" DONE\n");
+
+        // Free memory
+        stbi_image_free(inputData);
+        stbi_image_free(outputConvolution);
+        stbi_image_free(outputMaxPool);
+        stbi_image_free(outputMinPool);
+        stbi_image_free(outputAvgPool);
     }
-
-    // Open image
-    int width, height, componentCount;
-    printf("Loading png file...\r\n");
-    unsigned char* inputData = stbi_load(argv[1], &width, &height, &componentCount, 4);
-    if (!inputData)
-    {
-        printf("Failed to open image\r\n");
-        return -1;
-    }
-    printf(" DONE \r\n" );
-
-    // Convolution on CPU
-    unsigned char* outputConvolution = (unsigned char*) malloc(sizeof(unsigned char) * (width - 2) * (height - 2) * 4);
-    printf("Applying convolution...\r\n");
-    convoluteCPU(inputData, outputConvolution, width, height, gaussianBlur);
-    printf(" DONE \r\n");
-
-    // Pooling on GPU
-    int poolWidth = (int)(width / POOLSTRIDE);
-    int poolHeight = (int)(height / POOLSTRIDE);
-    unsigned char* outputMaxPool = (unsigned char*) malloc(sizeof(unsigned char) * poolWidth * poolHeight * 4);
-    unsigned char* outputMinPool = (unsigned char*) malloc(sizeof(unsigned char) * poolWidth * poolHeight * 4);
-    unsigned char* outputAvgPool = (unsigned char*) malloc(sizeof(unsigned char) * poolWidth * poolHeight * 4);
-    printf("Pooling...\r\n");
-    pool(inputData, outputMaxPool, outputMinPool, outputAvgPool, width, height, POOLSTRIDE);
-    printf(" DONE \r\n");
-
-    // Write images back to disk
-    printf("Writing pngs to disk...\r\n");
-    stbi_write_png("convolution.png", width - 2, height - 2, 4, outputConvolution, 4 * width);
-    stbi_write_png("maxpool.png", poolWidth, poolHeight, 4, outputMaxPool, 4 * poolWidth);
-    stbi_write_png("minpool.png", poolWidth, poolHeight, 4, outputMinPool, 4 * poolWidth);
-    stbi_write_png("avgpool.png", poolWidth, poolHeight, 4, outputAvgPool, 4 * poolWidth);
-    printf(" DONE\r\n");
-
-    // Copy data to the gpu
-    /*printf("Copy data to GPU...\r\n");
-    unsigned char* ptrImageDataGpu = nullptr;
-    cudaMalloc(&ptrImageDataGpu, width * height * 4);
-    cudaMemcpy(ptrImageDataGpu, inputData, width * height * 4, cudaMemcpyHostToDevice);
-    printf(" DONE \r\n");*/
-
-    // Process image on gpu
-    /*printf("Running CUDA Kernel...\r\n");
-    dim3 blockSize(32, 32);
-    dim3 gridSize(width / blockSize.x, height / blockSize.y);
-    //convoluteGPU<<<gridSize, blockSize>>>(ptrImageDataGpu);
-    printf(" DONE \r\n" );*/
-
-    // Copy data from the gpu
-    /*printf("Copy data from GPU...\r\n");
-    cudaMemcpy(inputData, ptrImageDataGpu, width * height * 4, cudaMemcpyDeviceToHost);
-    printf(" DONE \r\n");*/
-
-    // Free memory
-    //cudaFree(ptrImageDataGpu);
-    stbi_image_free(inputData);
-    stbi_image_free(outputConvolution);
-    stbi_image_free(outputMaxPool);
-    stbi_image_free(outputMinPool);
-    stbi_image_free(outputAvgPool);
 }
